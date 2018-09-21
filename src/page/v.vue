f2ca90b8c0e148d21f56712e8f45f900ac9f6ebb
Soojung Lee <sooojungee@gmail.com>
2018-09-21 19:33:18 +0900

    180921

--- src/page/v.vue
+++ src/page/v.vue
@@ -1,36 +1,73 @@
 <template lang="pug">
-  div {{count}}
-    .button(@click="increase()" :disabled="maximumTen") increase
-    .button(@click="decrease()" :disabled="minimumZero") decrease
+  div#watch-example
+    .content
+      p yes/no 질문을 물어보세요:
+      input(v-model="question")
+      p {{ answer }}
+    .content
+      input(v-model="changeName" placeholder='fullName')
+      input(v-model="firstName" placeholder='firstName')
+      input(v-model="lastName" placeholder='lastName')
+      .text firstName: {{ firstName }}
+      .text lastName: {{ lastName }}
+      button(@click="change") click
+      .text fullName: {{ fullName }}
 </template>
 
 <script>
+import * as _ from 'lodash';
+import axios from 'axios';
+
 export default {
   name: 'v',
   data() {
     return {
-      count: 0
+      question: '',
+      answer: '질문을 하기 전까지는 대답할 수 없습니다.',
+      firstName: '',
+      lastName: '',
+      changeName: ''
     };
   },
+  watch: {
+    question() {
+      this.answer = '입력을 기다리는 중..';
+      this.getAnswer();
+    }
+  },
   computed: {
-    maximumTen() {
-      return this.count < 10;
-    },
-    minimumZero() {
-      return this.count > 0;
+    fullName: {
+      get() {
+        return `${this.firstName} ${this.lastName}`;
+      },
+      set(newValue) {
+        const names = newValue.split(' ');
+        this.firstName = names[0];
+        this.lastName = names[names.length - 1];
+      }
     }
   },
   methods: {
-    increase() {
-      if (this.maximumTen) {
-        this.count += 1;
-      }
+    change() {
+      console.log('1', this.fullName, typeof this.fullName);
+      this.fullName = this.changeName;
     },
-    decrease() {
-      if (this.minimumZero) {
-        this.count -= 1;
-      }
-    }
+    getAnswer: _.debounce(
+      function () {
+        if (this.question.indexOf('?') === -1) {
+          this.answer = '질문에는 일반적으로 물음표가 포함됩니다.';
+          return;
+        }
+        this.answer = '생각중';
+        const that = this;
+        axios.get('https://yesno.wtf/api')
+          .then((response) => {
+            that.answer = _.capitalize(response.data.answer);
+          })
+          .catch((error) => {
+            that.answer = `에러! ${error}`;
+          });
+      }, 500)
   }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ export default new Router({
     {
       path: '/test',
       name: 'test',
-      component: test.default
+      component: test
     },
     {
       path: '/crawling',
